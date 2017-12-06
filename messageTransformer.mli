97d5e6fd2dfedc5063ba62da121891c9e79c1ebc
Pippen Wu <hw473@cornell.edu>
2017-12-06 14:51:44 -0500

    Add files via upload
    updated .mli

---
+++ messageTransformer.mli
@@ -0,0 +1,23 @@
+(* An [MessageTransformer] takes in messages and does something to the message,
+ * including spellchecking and replacing shortcuts *)
+
+    (* The type of a message *)
+    type msg
+    (* The type of a word *)
+    type word
+    (* The type of a dictionary *)
+    type dict
+
+    (* [send] checks whether all the words in a message is "valid",
+     * i.e., it passes the spellcheck test and is not a defined shortcut.
+     * It alters the message until all the words are valid. *)
+    val send : msg -> msg
+
+    (* [def] defines a user defined shortcut *)
+    val def : word -> word -> unit
+
+    (* [pm] encyrpts a message *)
+    val pm : msg -> msg
+
+    (* [depm] decyrpts a message *)
+    val depm : msg -> msg
