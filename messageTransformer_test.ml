3fcc440fe7e4c1ee74e1403fc5c966349ae52723
Pippen Wu <hw473@cornell.edu>
2017-12-06 18:25:49 -0500

    updated test suite

--- messageTransformer_test.ml
+++ messageTransformer_test.ml
@@ -8,6 +8,9 @@ open MessageTransformer
 let send_tests = [
   "apple" >:: (fun _ -> assert_equal "apple"  (send "apple"));
   "banana" >:: (fun _ -> assert_equal "banana"  (send "banana"));
+  "rotten banana" >:: (fun _ -> assert_equal "banana"  (send "nanana"));
+  "run fast" >:: (fun _ -> assert_equal "got to go"  (send "gtg"));
+  "awesome!" >:: (fun _ -> assert_equal "don't forget to be awesome"  (send "dftba"));
 ]
 
 
